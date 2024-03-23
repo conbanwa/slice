bd08e19e549f145175a7207a2fede05e5dfce59a
conbanwa <35380552+conbanwa@users.noreply.github.com>
2024-03-23 21:51:38 +0800

    Update slice_test.go

--- slice_test.go
+++ slice_test.go
@@ -13,16 +13,19 @@ func TestSlice(t *testing.T) {
 	assert.Equal(t, string(Slice([]byte(`1234567890`), -2)), "90")
 	assert.Equal(t, string(Slice([]byte(`1234567890`), 0, -4)), "123456")
 	assert.Equal(t, string(Slice([]byte(`1234567890`), 2, -4)), "3456")
-	assert.Equal(t, string(Slice([]byte(`1234567890`), -5, 4)), "")
 	assert.Equal(t, string(Slice([]byte(`1234567890`), 6, 99)), "7890")
 	assert.Equal(t, string(Slice([]byte(`1234567890`), -5, 99)), "67890")
 	assert.Equal(t, string(Slice([]byte(`1234567890`), -99, 99)), "1234567890")
-	assert.Equal(t, BodyCut(longBytes),
-		"package lap\n\nimport (\n\t\"sync/atomic\"\n\t\"time\"\n\n\t\"github.com/conbanwa/logs\"\n)\n\ntype Elapse []int64\nty")
-	assert.Equal(t, BodyCut([]byte("package lap\n\nimport (\n\t\"sync/atomic"), 7), "package")
+	assert.Equal(t, string(Slice([]byte(`1234567890`), -5, 4)), "")
+	assert.Equal(t, string(Slice([]byte(`1234567890`), 99, -99)), "")
+	assert.Equal(t, string(Slice([]byte(`1234567890`), 5, -99)), "")
+}
+
+func TestCut(t *testing.T) {
 	assert.Equal(t, Cut(longBytes),
 		String2Bytes("package lap\n\nimport (\n\t\"sync/atomic\"\n\t\"time\"\n\n\t\"github.com/conbanwa/logs\"\n)\n\ntype Elapse []int64\nty"))
 	assert.Equal(t, Cut([]byte("package lap\n\nimport (\n\t\"sync/atomic"), 7), String2Bytes("package"))
+
 }
 
 func unique[T comparable](sl *[]T) {
